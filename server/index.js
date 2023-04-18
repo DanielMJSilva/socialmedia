2571ee030421ce271a0d69b7cd3d96e8464f8501
dmachad7 <113929117+dmachad7@users.noreply.github.com>
2023-04-18 13:59:54 -0400

    added authorization middleware

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
-import { register } from "./controllers/auth.js"
+import { register } from "./controllers/auth.js";
+//import { verifyToken } from "./middleware/auth.js";
 
 // CONFIGURATIONS
 
